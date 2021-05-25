1eac37ad5c7b66693ed1fbe443f3e7e0440e7558
dObbOb11 <jackbox@asnowdon.uk>
2021-05-25 19:57:00 +0100

    added the pymenu module
    My custom pymenu module, I copied the code into the main.py file because it is so small and i could not get it to import from the dependencies directory.

--- .source/main.py
+++ .source/main.py
@@ -1,3 +1,28 @@
-from dependencies import pymenu
+# My custom pymenu module, I copied the code here because it is so small and i could not get it to import from the dependencies directory.
 
-rootModes = menu()
+def menu():
+  return []
+def menuItem(name):
+  return name
+def itemAppend(menuName, item):
+  menuName.append(item)
+def show(menuName):
+  optionNum = 0
+  for item in menuName:
+    optionNum += 1
+    print("[" + str(optionNum) + "]", item)
+  option = input("\n>> ")
+  try:
+    return int(option) - 1
+  except:
+    return "err"
+
+rootModesMenu = menu()
+
+rootModes = {
+    "testOp": "Test Option"
+}
+
+itemAppend(rootModesMenu, rootModes)
+
+rootMode = show(rootModesMenu)
